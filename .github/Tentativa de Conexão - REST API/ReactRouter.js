6f7fc9282a34a5a4a139d5b1888b4c830607546c
Rodrigo <rodrigo.campos.mim@gmail.com>
2024-03-21 00:43:39 -0300

    att

--- .github/Tentativa de Conexão - REST API/ReactRouter.js
+++ .github/Tentativa de Conexão - REST API/ReactRouter.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import pagina_inicial from './pagina_inicial'
 import InserirExame from './InserirExame';
 import CadastrarExame from './CadastrarExame';
-import UploadResultado from './UploadResultado';
 import ListarExames from './ListarExames';
 
 // Trocar o nome das variáveis de importação //
@@ -14,8 +13,6 @@ function App() {
             <div>
                 <Switch>
                     <Route exact path="/pagina_inicial" component={pagina_inicial} />
-                    <Route exact path="/inserir_exame" component={InserirExame} />
-                    <Route exact path="/upload_resultado" component={UploadResultado} />
                     <Route exact path="/listar_exames" component={ListarExames} />
                     <Route exact path="/cadastrar_exame" component={CadastrarExame} />
                 </Switch>
---
+++ .github/Tentativa de Conexão - REST API/pagina_inicial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PaginaInicial() {
+    return (
+        <div>
+            <h2>Página Inicial</h2>
+            <p>Bem-vindo ao nosso aplicativo de gerenciamento de exames!</p>
+        </div>
+    );
+}
+
+export default PaginaInicial;
